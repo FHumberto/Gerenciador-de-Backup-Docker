595c5654cbbbb122ca31740faaae88c9ee7d9b65
Humberto Guedes <franciscohguedes@hotmail.com>
2023-11-17 04:31:12 -0300

    Add: Algorítimo para lidar com arquivos existente na pasta cloud, mover e manter os últimos dois arquivos recentes de bkp

--- src/SSV.Console/Program.cs
+++ src/SSV.Console/Program.cs
@@ -12,7 +12,12 @@ Settings? settings = config.GetRequiredSection("Settings").Get<Settings>();
 
 try
 {
-    string diretorioTemporario = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "temp");
+    string diretorioTemporario = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "temp\\");
+
+    if (!Directory.Exists(diretorioTemporario))
+    {
+        Directory.CreateDirectory(diretorioTemporario);
+    }
 
     if (settings?.DiretoriosDosVolumes?.Count > 0)
     {
@@ -22,18 +27,40 @@ try
             {
                 if (Directory.Exists(volumePath))
                 {
-                    Directory.CreateDirectory(diretorioTemporario);
-
                     //! Adiciona a data formatada ao nome do arquivo ZIP
                     string dataFormatada = DateTime.Now.ToString("dd-MM-yyyy-HHmmss");
                     string nomeArquivoZip = $"{diretorioTemporario}{volumeAlias}-{dataFormatada}.zip";
 
-                    // Cria o arquivo zipado
+                    //! Cria o arquivo zipado
                     ZipFile.CreateFromDirectory(volumePath, nomeArquivoZip);
 
+                    //! Vasculha o diretório cloud e remove os arquivos antigos (mantendo os dois últimos)
                     if (Directory.Exists(settings.DiretorioCloud))
                     {
-                        // TODO: IMPLEMENTAR A LÓGICA DE MOVER ARQUIVO E MANTER O PENULTIMO BACKUP
+                        string[] files = Directory.GetFiles(settings.DiretorioCloud, "*", SearchOption.AllDirectories);
+
+                        List<string> filesToRemove = [];
+
+                        foreach (string file in files)
+                        {
+                            if (file.Contains(volumeAlias) && file.EndsWith(".zip"))
+                            {
+                                filesToRemove.Add(file);
+                            }
+                        }
+
+                        if (filesToRemove.Count > 1)
+                        {
+                            filesToRemove.Sort();
+                            filesToRemove.Reverse();
+
+                            for (int i = 1; i < filesToRemove.Count; i++)
+                            {
+                                File.Delete(filesToRemove[i]);
+                            }
+                        }
+
+                        File.Move(nomeArquivoZip, Path.Combine(settings.DiretorioCloud, Path.GetFileName(nomeArquivoZip)));
                     }
                     else
                     {
